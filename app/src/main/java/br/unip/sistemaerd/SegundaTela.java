2368c60f6a963f5081fa26522b130b9365db9030
erika <hELO300816>
2020-11-01 19:52:22 -0300

    Add tela de cadastro

--- app/src/main/java/br/unip/sistemaerd/SegundaTela.java
+++ app/src/main/java/br/unip/sistemaerd/SegundaTela.java
@@ -2,7 +2,10 @@ package br.unip.sistemaerd;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SegundaTela extends AppCompatActivity {
 
@@ -10,5 +13,63 @@ public class SegundaTela extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda_tela1);
+
+        Button btInserir = (Button) findViewById(R.id.btInserir);
+        btInserir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                irTelaCadastro();
+            }
+        });
+
+        Button btSair = (Button) findViewById(R.id.btSair);
+        btSair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                irTelaInicial();
+            }
+        });
+
+
     }
+
+    private void irTelaCadastro(){
+
+        Intent intent = new Intent(this, TelaCadastro.class);
+        startActivity(intent);
+
+    }
+
+      private void irTelaInicial() {
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+
 }
+
+
+/*    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btLogin = (Button) findViewById(R.id.btLogin);
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView tLogin = (TextView) findViewById(R.id.Login);
+                TextView tSenha = (TextView) findViewById(R.id.tSenha);
+                String login = tLogin.getText().toString();
+                String senha = tSenha.getText().toString();
+                if(login.equals("Pedro")&&senha.equals("123")){
+                    alert("Login realizado com sucesso");
+                    irParaSegundaTela();
+                }else{
+                    alert("Login ou senha invalida");
+                };
+            }
+        });
+    }*/
+
+
--- app/src/main/java/br/unip/sistemaerd/TelaCadastro.java
+++ app/src/main/java/br/unip/sistemaerd/TelaCadastro.java
@@ -3,12 +3,27 @@ package br.unip.sistemaerd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class TelaCadastro extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_cadastro);
+
+
+
+        Spinner spinner = (Spinner) findViewById(R.id.estadosBrasileiros);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.estadosBrasilerios, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 }
